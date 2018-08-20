9b5a6c24ae17ee1e0cdb90b91b63c422fbf23b6e
Jeff Lee <jcjl013@gmail.com>
2018-08-20 15:29:09 -0700

    Initial Xcode commit

---
+++ Seven Wonders Scorer/Seven Wonders Scorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seven Wonders Scorer
+//
+//  Created by Jeff Lee on 8/20/18.
+//  Copyright © 2018 Jeff Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
